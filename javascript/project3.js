52999c5a0b803f85bf128cfd8b0fb43b9497e9d8
Justin Wetzel <jwetzel22@gmail.com>
2016-02-28 17:52:30 -0500

    Update project3.js

--- javascript/project3.js
+++ javascript/project3.js
@@ -1,5 +1,6 @@
 // Euler Project #3
-/** The prime factors of 13195 are 5, 7, 13 and 29.
+/** 
+ * The prime factors of 13195 are 5, 7, 13 and 29.
  * What is the largest prime factor of the number 600851475143 ?
  */
 // number may take a while to interpret, works with smaller numbers just fine
