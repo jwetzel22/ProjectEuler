cd23fbb039baac7a29d67ca8982313aa04a8c0fc
Justin Wetzel <jwetzel22@gmail.com>
2016-02-28 17:51:50 -0500

    Added files via upload
    first 3 problems with js

---
+++ javascript/project3.js
@@ -0,0 +1,26 @@
+// Euler Project #3
+/** The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+// number may take a while to interpret, works with smaller numbers just fine
+var max = 13195;
+var numList = [];
+
+function prime(num) {
+    for(var i = 2; i <= Math.ceil(Math.sqrt(num)); i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+for(var x = 2; x < max; x++) {
+    if(max % x === 0) {
+        if(prime(x)) {
+            numList.push(x);
+        }
+    }
+}
+var l = numList.length;
+console.log(numList[l-1]);
